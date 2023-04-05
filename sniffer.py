966e5b7c25d4642e5269b1aefc7b4f47e169876a
Moises4343 <78661793+Moises4343@users.noreply.github.com>
2023-04-04 23:32:37 -0600

    Add files via upload

--- sniffer.py
+++ sniffer.py
@@ -1,5 +1,6 @@
 from scapy.all import *
 import mysql.connector
+import datetime
 
 #connection to database
 coneccion = mysql.connector.connect(user='root', password='193243up', host='127.0.0.1', database='CS1')
@@ -10,7 +11,7 @@ print(coneccion)
 cursor=coneccion.cursor()
 
 #sql command
-add_all=("INSERT INTO sniff(mac_src,mac_des,ip_src,tam_src,ip_des,tam_des) VALUES (%s,%s,%s,%s,%s,%s)")
+add_all=("INSERT INTO sniff(mac_src,mac_des,ip_src,tam_src,ip_des,tam_des,date_time) VALUES (%s,%s,%s,%s,%s,%s,%s)")
 
 # callback function - called for every packet
 def traffic_monitor_callbak(pkt):
@@ -24,6 +25,10 @@ def traffic_monitor_callbak(pkt):
         mac_src=pkt.src
         mac_des=pkt.dst
 
+       
+#get the current date and time
+        now = datetime.datetime.now() 
+
 #print on console the data got  from the sniffers
         print (ip_src)
         print (tam_ip_src)
@@ -31,9 +36,10 @@ def traffic_monitor_callbak(pkt):
         print (tam_ip_des)
         print (mac_src)
         print (mac_des)
+        print (now)
 
 #commit the data to db
-        cursor.execute(add_all, (mac_src,mac_des,ip_src,tam_ip_src,ip_des,tam_ip_des,))
+        cursor.execute(add_all, (mac_src,mac_des,ip_src,tam_ip_src,ip_des,tam_ip_des,now,))
         coneccion.commit()
 
 # capture traffic
